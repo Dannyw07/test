7ec4726f487236143df54adda0a6ef666f5c6aec
Dannyw07 <106722381+Dannyw07@users.noreply.github.com>
2023-02-09 03:53:43 +0800

    forth commit

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
       <h1>wadup</h1>
+      <Homepage />
     </div>
   );
 }
