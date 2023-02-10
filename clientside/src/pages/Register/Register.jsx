a48ba303e9748450915f68c94c6f8d85640f5878
Dannyw07 <106722381+Dannyw07@users.noreply.github.com>
2023-02-10 13:47:35 +0800

    update register files

--- clientside/src/pages/Register/Register.jsx
+++ clientside/src/pages/Register/Register.jsx
@@ -1,12 +1,53 @@
 import React from "react";
 import "../Register/register.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { MuiTelInput } from "mui-tel-input";
 
 const Register = () => {
+  const [phone, setPhone] = React.useState("");
+
+  const handleChange = (newPhone) => {
+    setPhone(newPhone);
+  };
   return (
     <div className="reg">
       <div className="registerContainer">
         <div className="regTitle">
           <h1>Create an Acccount</h1>
+        </div>
+        <div className="regForm">
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": {
+                m: 1,
+                width: "97%",
+                display: "grid",
+                gridTemplateRows: "repeat(1, 1fr)",
+              },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField id="filled-basic" label="Username" variant="filled" />
+            <TextField id="filled-basic" label="Email" variant="filled" />
+            <MuiTelInput
+              onlyCountries={["MY"]}
+              value={phone}
+              onChange={handleChange}
+            />
+            <TextField id="filled-basic" label="Password" variant="filled" />
+            <TextField
+              id="filled-basic"
+              label="Confirm Password"
+              variant="filled"
+            />
+          </Box>
+        </div>
+
+        <div className="regButton">
+          <button className="regB">Continue</button>
         </div>
       </div>
     </div>
