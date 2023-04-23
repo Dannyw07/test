31ce4c5269b5d727647be6741cbc669c26be0f9a
Dannyw07 <106722381+Dannyw07@users.noreply.github.com>
2023-04-24 02:18:14 +0800

    new updated file

--- clientside/src/pages/News/News.js
+++ clientside/src/pages/News/News.js
@@ -39,13 +39,49 @@ const News = () => {
         </ul>
       </nav> */}
 
-      <div className="Picture bg-secondary " style={{ height: "100%" }}>
+      <div className="Picture  ">
         <Image
           src="https://image.deemples.com/wp-content/uploads/2018/06/tourism-malaysia.jpg"
           width="90%"
           height="500px"
           alt="description of the image"
         />
+
+        <div className="content bg-light mx-5 min-vh-100 d-flex justify-content-between g-0 ">
+          <div
+            class="col-4 mt-5 mx-5 align-items-center bg-primary  rounded-3"
+            style={{ height: "700px", width: "400px" }}
+          >
+            <Image
+              src="https://cdn.shopify.com/s/files/1/0512/8709/7512/products/astrox100game2_1200x1200.jpg?v=1644920331"
+              width="400px"
+              height="500px"
+              alt="description of the image"
+            />
+          </div>
+          <div
+            class="col-4 mt-5 mx-5  align-items-center bg-primary  rounded-3"
+            style={{ height: "700px", width: "400px" }}
+          >
+            <Image
+              src="https://cdn.shopify.com/s/files/1/0512/8709/7512/products/astrox100game1_480x.jpg?v=1644920320"
+              width="400px"
+              height="500px"
+              alt="description of the image"
+            />
+          </div>
+          <div
+            class="col-4 mt-5 mx-5  align-items-center bg-primary rounded-3"
+            style={{ height: "700px", width: "400px" }}
+          >
+            <Image
+              src="https://cdn.shopify.com/s/files/1/0512/8709/7512/products/astrox100game_480x.jpg?v=1644920305"
+              width="400px"
+              height="500px"
+              alt="description of the image"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
