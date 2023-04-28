3aa7fb021bc3a570a69d45c0735829c20eb02f4a
Dannyw07 <106722381+Dannyw07@users.noreply.github.com>
2023-04-29 01:28:22 +0800

    footer

--- clientside/src/components/Footer/Footer.jsx
+++ clientside/src/components/Footer/Footer.jsx
@@ -4,7 +4,19 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="bg-white m-5 ">
-        <h4>test</h4>
+        <div className="footer-area-1 col-md-3 col-sm-6 ">
+          <section className="widget text-2">
+            <div className="widget-inner ">
+              <h3 className="widget-title">About Kinabalu Golf</h3>
+              <div className="text-align-left d-block ">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                  vitae ornare massa, at consequat ipsum. Sed ullamcorper.
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
